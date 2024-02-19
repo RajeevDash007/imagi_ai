322e181ef271ecc0730136b05084b310d2b8dbb7
Rajeev <rajeevdash07@gmail.com>
2024-02-19 11:58:14 +0530

    middleware implemented successfully

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/anyone-can-visit-this-route"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/no-auth-in-this-route"],
-});
+export default authMiddleware({});
 
 export const config = {
   // Protects all routes, including api/trpc.
