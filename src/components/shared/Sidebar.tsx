91248018012d5bda9653aa4954b00ff8c6d1feed
Rajeev <rajeevdash07@gmail.com>
2024-02-21 19:47:40 +0530

    mapping of the li items from constants to navbar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,14 +1,38 @@
+'use client'
+
+import { navLinks } from '@/constants'
+import { SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4'>
             <Link href="/" className='sidebar-logo'>
                 <Image src="/assets/images/logo-text.svg" alt='logo' width={180} height={28}/>
             </Link>
+
+            <nav className='sidebar-nav'>
+              <SignedIn>
+                <ul className='sidebar-nav_elements'>
+                    {navLinks.map((link)=>{
+                      const isActive = link.route === pathname
+
+                      return(
+                        <li key={link.route} className={`sidebar-nav_element group  ${
+                          isActive ? 'bg-purple-gradient text-white' : 'text-gray-700'
+                        }`}>
+                            {link.label}
+                        </li>
+                      )
+                    })}
+                </ul>
+              </SignedIn>
+            </nav>
         </div>
     </aside>
   )
