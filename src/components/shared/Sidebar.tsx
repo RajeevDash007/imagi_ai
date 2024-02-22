f1963a7702864a738d4dd653b02f495d66f5a02f
Rajeev <rajeevdash07@gmail.com>
2024-02-22 18:18:05 +0530

    added Signout sidebar and added profile section in sidebar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { navLinks } from '@/constants'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const Sidebar = () => {
   const pathname = usePathname();
@@ -35,8 +36,17 @@ const Sidebar = () => {
                         </li>
                       )
                     })}
+
+                    <li className='flex-center cursor-pointer gap-2 p-4'>
+                      <UserButton afterSignOutUrl='/' showName/>                    </li>
                 </ul>
               </SignedIn>
+
+              <SignedOut>
+                <Button asChild className='button bg-purple-gradient bg-cover'>
+                  <Link href="/sign=in">Login</Link>
+                </Button>
+              </SignedOut>
             </nav>
         </div>
     </aside>
