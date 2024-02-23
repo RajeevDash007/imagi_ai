670308223bd1a98c99a97f573546b83266c8b5f0
Rajeev <rajeevdash07@gmail.com>
2024-02-23 19:59:29 +0530

    implementation of mobilenav

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
@@ -6,6 +7,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     <main className='root'>
         {/* <Sidebar /> */}
         <Sidebar />
+        <MobileNav />
         <div className="root-container">
             <div className="wrapper">
                 {children}
