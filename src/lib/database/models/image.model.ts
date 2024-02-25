39536b8506055806e3fac79235a362c8af5cfd57
Rajeev <rajeevdash07@gmail.com>
2024-02-25 10:25:56 +0530

    included ImageSchema

---
+++ src/lib/database/models/image.model.ts
@@ -0,0 +1,19 @@
+import { Schema } from "mongoose";
+import { uptime } from "process";
+
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
